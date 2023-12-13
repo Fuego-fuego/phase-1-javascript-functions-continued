760632fb383fffe417e6070027f56e9034b0c493
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-13 08:43:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 // code your solution here
 
-// creating a function using function declaration 
+// Creating a function using function declaration 
 function saturdayFun (activity =`roller-skate` ){
 
     return `This Saturday, I want to ${activity}!`
 }
 
 
-// Assigning a function expression to a  constant 
+// Assigning a function expression to a constant 
 const mondayWork = function (activity = `go to the office`){
 
         return `This Monday, I will ${activity}.`;
 }
 
-// closure 
-
+// Closure in f(x)s
 function wrapAdjective(decorativeSymbol){
 
     return function (opinion = `special`){
