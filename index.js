2b904b0f16bb56e0af49d61b01d8fa217afece5d
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-13 08:38:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+// creating a unction using function declaration 
+function saturdayFun (activity =`roller-skate` ){
+
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+// Assigning a function expression to a  constant 
+const mondayWork = function (activity = `go to the office`){
+
+        return `This Monday, I will ${activity}.`;
+}
+
+// closure 
+
+function wrapAdjective(decorativeSymbol){
+
+    return function (opinion = `special`){
+
+        return `You are ${decorativeSymbol+opinion+decorativeSymbol}!`
+    };
+};
